5d72907fc730e81e20a8db04df4984d7c3eb5d46
Himanshu6165 <97308481+Himanshu6165@users.noreply.github.com>
2022-01-22 21:28:06 +0530

    Add files via upload

---
+++ output_asg2.py
@@ -0,0 +1,30 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================================================================== RESTART: C:/Users/Dell/OneDrive/Desktop/asg2.py ===================================================================
+35
+egaugnal evitisnes esac a si nohtyp
+a case sensitive
+python is object oriented language
+10
+pythonisacasesensitivelanguage
+hey, Himanshu Bansal here!
+ My sid is 21105050
+ I am from electronics and communication department
+ my cgpa is 9.9
+
+a&b: 8
+a|b: 58
+a^b: 50
+a<<2: 224 	b<<2: 40
+a>>2: 14 	b>>2: 0
+enter the 1st no:25
+enter the second no:56
+enter the 3rd no:100
+the highest number is: 100
+Enter input string:hey everyone
+no
+enter the 1st lenght:5
+enter the 2nd number:6
+enter the 3rd number:4
+yes
