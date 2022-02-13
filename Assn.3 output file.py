f7e174188f3b9ac92e4c6dfb156f261790d67eab
Himanshu6165 <97308481+Himanshu6165@users.noreply.github.com>
2022-02-13 22:40:45 +0530

    Add files via upload

---
+++ Assn.3 output file.py
@@ -0,0 +1,73 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/Dell/OneDrive/Desktop/Assignment3.py ============
+Question 1
+Please give an input:Banana
+Total occurences
+b-1
+a-3
+n-2
+question 2
+please enter day:21
+please enter month:07
+please enter year:2003
+Next Date is:22/7/2003
+Question 3
+[5, 7, 19, 24]
+[(5, 25), (7, 49), (19, 361), (24, 576)]
+question 4
+enter grade points:7
+Your Grade is B and Good performance
+question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+Question 6
+Please Enter SID of Student: 21105050
+Please enter the name of the Student: Himanshu
+If you want to enter more details press Y/N:Y
+Please Enter SID of Student: 21105051
+Please enter the name of the Student: XYZ
+If you want to enter more details press Y/N:N
+Part a
+21105050 is Himanshu
+21105051 is XYZ
+
+Part b
+value sorted dictionary is ['Himanshu', 'XYZ']
+
+Part c
+Keys sorted dictionary is [21105050, 21105051]
+
+Part D
+Please enter the student's SID whose detail you need- 21105050
+Name of the required student is Himanshu
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 9
+Fibonnaci sequence with 9 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+Average of total 9 terms of sequence is 6.0
+END
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
