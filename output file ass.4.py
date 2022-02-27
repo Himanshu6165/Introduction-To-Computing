b0f15991798afb39a015086bb8f82ea96462ae82
Himanshu6165 <97308481+Himanshu6165@users.noreply.github.com>
2022-02-27 21:33:15 +0530

    Add files via upload

---
+++ output file ass.4.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/Dell/OneDrive/Desktop/assignment4.py ============
+Question no.1
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+Question no.2
+Enter the number of rows in Pascal's Triangle: 6
+
+Using recursions:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Using loops:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Question no.3
+Enter the first integer: 15
+Enter the first integer: 4
+Remainder is:  3
+Quotient is:  3
+Both values are non zero
+{7}
+{7}
+{8, 7}
+{8, 7}
+{8, 9, 7}
+{8, 9, 7}
+{8, 9, 7}
+Immutable set:  frozenset({8, 9, 7})
+Largest value in the set:  9
+Hash value:  9
+
+Question no.4
+Object created
+The name of the student is Himanshu Bansal and SID is 21105050.
+Object destroyed
+
+Question no.5
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+Question no.6
+Enter the first word: left
+
+Enter a new meaningful word to test your friendship: felt
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
+
